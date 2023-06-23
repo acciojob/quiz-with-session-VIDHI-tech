e157cc5a2139788f6b5aa0f97acb72de9b784251
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-23 09:49:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-//your JS code here.
+const questions = [
+  {
+    question: "What is the capital of France?",
+    choices: ["Paris", "London", "Berlin", "Madrid"],
+    answer: "Paris",
+  },
+  {
+    question: "What is the highest mountain in the world?",
+    choices: ["Everest", "Kilimanjaro", "Denali", "Matterhorn"],
+    answer: "Everest",
+  },
+  {
+    question: "What is the largest country by area?",
+    choices: ["Russia", "China", "Canada", "United States"],
+    answer: "Russia",
+  },
+  {
+    question: "Which is the largest planet in our solar system?",
+    choices: ["Earth", "Jupiter", "Mars"],
+    answer: "Jupiter",
+  },
+  {
+    question: "What is the capital of Canada?",
+    choices: ["Toronto", "Montreal", "Vancouver", "Ottawa"],
+    answer: "Ottawa",
+  },
+];
+
 // Check if session storage is supported
 function isSessionStorageSupported() {
   try {
@@ -54,57 +81,77 @@ function saveScore(score) {
     localStorage.setItem('score', score.toString());
   }
 }
-// Do not change code below this line
-// This code will just display the questions to the screen
-const questions = [
-  {
-    question: "What is the capital of France?",
-    choices: ["Paris", "London", "Berlin", "Madrid"],
-    answer: "Paris",
-  },
-  {
-    question: "What is the highest mountain in the world?",
-    choices: ["Everest", "Kilimanjaro", "Denali", "Matterhorn"],
-    answer: "Everest",
-  },
-  {
-    question: "What is the largest country by area?",
-    choices: ["Russia", "China", "Canada", "United States"],
-    answer: "Russia",
-  },
-  {
-    question: "Which is the largest planet in our solar system?",
-    choices: ["Earth", "Jupiter", "Mars"],
-    answer: "Jupiter",
-  },
-  {
-    question: "What is the capital of Canada?",
-    choices: ["Toronto", "Montreal", "Vancouver", "Ottawa"],
-    answer: "Ottawa",
-  },
-];
 
 // Display the quiz questions and choices
 function renderQuestions() {
+  const questionsElement = document.getElementById('questions');
+  const progress = retrieveProgress();
+
   for (let i = 0; i < questions.length; i++) {
     const question = questions[i];
-    const questionElement = document.createElement("div");
+    const questionElement = document.createElement('div');
     const questionText = document.createTextNode(question.question);
     questionElement.appendChild(questionText);
+
     for (let j = 0; j < question.choices.length; j++) {
       const choice = question.choices[j];
-      const choiceElement = document.createElement("input");
-      choiceElement.setAttribute("type", "radio");
-      choiceElement.setAttribute("name", `question-${i}`);
-      choiceElement.setAttribute("value", choice);
-      if (userAnswers[i] === choice) {
-        choiceElement.setAttribute("checked", true);
+      const choiceElement = document.createElement('input');
+      choiceElement.setAttribute('type', 'radio');
+      choiceElement.setAttribute('name', `question-${i}`);
+      choiceElement.setAttribute('value', choice);
+
+      if (progress[i] === choice) {
+        choiceElement.setAttribute('checked', true);
       }
+
+      choiceElement.addEventListener('change', saveProgress);
+
       const choiceText = document.createTextNode(choice);
-      questionElement.appendChild(choiceElement);
-      questionElement.appendChild(choiceText);
+      const choiceLabel = document.createElement('label');
+      choiceLabel.appendChild(choiceElement);
+      choiceLabel.appendChild(choiceText);
+
+      questionElement.appendChild(choiceLabel);
     }
+
     questionsElement.appendChild(questionElement);
   }
 }
+
+// Calculate the score
+function calculateScore() {
+  const choices = document.querySelectorAll('input[type="radio"]:checked');
+  let score = 0;
+
+  for (let i = 0; i < choices.length; i++) {
+    const choice = choices[i].value;
+    const answer = questions[i].answer;
+
+    if (choice === answer) {
+      score++;
+    }
+  }
+
+  return score;
+}
+
+// Display the score
+function displayScore(score) {
+  const scoreElement = document.getElementById('score');
+  const scoreText = document.createTextNode(`Your score is ${score} out of ${questions.length}.`);
+  scoreElement.appendChild(scoreText);
+}
+
+// Handle the submit button click event
+function handleSubmit() {
+  const score = calculateScore();
+  saveScore(score);
+  displayScore(score);
+}
+
+// Attach event listener to the submit button
+const submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', handleSubmit);
+
+// Render the questions on page load
 renderQuestions();
